918a3f945abdb6fcfcf591fc74c60ca76ca2d399
Yasar-17 <mohammedyasar990@gmail.com>
2026-09-02 23:04:32 +0530

    Change sponsor logo object-fit to contain to prevent image cropping

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -671,7 +671,7 @@ export default function About() {
         .ws-sponsor-logo img {
           width: 100%;
           height: 100%;
-          object-fit: cover;
+          object-fit: contain;
         }
 
         .ws-sponsor-info {
